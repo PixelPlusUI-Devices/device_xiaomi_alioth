2d0e47d38215f4018af677b64daf39668df0003f
EmanuelCN <emanuelghub@gmail.com>
2022-07-24 18:35:49 +0200

    alioth: Set 1080p resolution for boot animation

--- aosp_alioth.mk
+++ aosp_alioth.mk
@@ -12,7 +12,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, vendor/aosp/config/common_full_phone.mk)
 WITH_GAPPS := true
 TARGET_INCLUDE_WIFI_EXT := true
-TARGET_BOOT_ANIMATION_RES := 1440
+TARGET_BOOT_ANIMATION_RES := 1080
 
 # PixelPlusUI Maintainer Flags
 PPUI_MAINTAINER := EmanuelCN
