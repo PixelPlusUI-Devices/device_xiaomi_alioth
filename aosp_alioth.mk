6bf352013d58b7b13b36fab6e8cdfb5c8895e57d
revolwoc <imsakshisharma04@gmail.com>
2022-10-23 03:21:16 +0000

    PpUi
    Signed-off-by: revolwoc <imsakshisharma04@gmail.com>

--- aosp_alioth.mk
+++ aosp_alioth.mk
@@ -9,7 +9,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
 # Inherit some common lineage stuff.
-$(call inherit-product, vendor/evolution/config/common_full_phone.mk)
+$(call inherit-product, vendor/aosp/config/common_full_phone.mk)
 
 # Device Specific Flags
 TARGET_INCLUDE_WIFI_EXT := true
@@ -21,12 +21,16 @@ TARGET_INCLUDE_STOCK_ARCORE := true
 TARGET_INCLUDE_LIVE_WALLPAPERS := true
 TARGET_SUPPORTS_QUICK_TAP  := true
 TARGET_USES_MINI_GAPPS := false
-EVO_BUILD_TYPE := revolwoc
+
+# PixelPlusUI Official Stuff
+CUSTOM_BUILD_TYPE := OFFICIAL
+IS_PHONE := true
+PPUI_MAINTAINER := revolwoc
 
 # Inherit from alioth device
 $(call inherit-product, device/xiaomi/alioth/device.mk)
 
-PRODUCT_NAME := evolution_alioth
+PRODUCT_NAME := aosp_alioth
 PRODUCT_DEVICE := alioth
 PRODUCT_MANUFACTURER := Xiaomi
 PRODUCT_BRAND := POCO
